7912bb8f47c329daf2b525df84a638f636b2c125
Lucas Fernandes Nunes Oliveira <152298428+lucasferdss@users.noreply.github.com>
2026-05-05 13:37:48 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +9,6 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </QueryClientProvider>
